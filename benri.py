045a68e21e680226f47ede10ab93b29185338a3f
“tyokinbako <haruharu0523.rururu.gmail.com>
2024-08-29 17:31:48 +0900

    “haru”

--- benri.py
+++ benri.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask import render_template
 haru = Flask(__name__)
+
+@haru.route("/kobiru")
+def okane():
+    return render_template("kobirunope-zi.html")
+
 @haru.route("/tyatto")
-def tatemono():
+def tyatto():
     return render_template("tyattonope-zi.html")
 haru.run(host="0.0.0.0")
